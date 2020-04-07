c31215d7c7ce5ee1ae018834250dc4f1ec5cb32f
Will Storey <will@willstorey.ca>
2020-04-07 15:35:08 -0400

    Fixed error in Admin Factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :admin, class: User do
-    sequence(:email) { |n| Faker::Internet.safe_email("admin_user#{n}") }
+    sequence(:email) { |n| Faker::Internet.safe_email(name: "admin_user#{n}") }
     password { 'test1234' }
     roles { 'Admin' }
   end
